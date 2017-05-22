5744ebb117e4613f26612076f4e40b1a7bda595b
jefhai <jefhai@users.noreply.github.com>
2017-05-22 12:25:39 -0400

    setup basic sockets

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
+const socketio = require('socket.io').listen(server)
+
+connections = []
+
+server.listen(3000, "0.0.0.0", function () {
+  console.log('Server running... http://localhost:3000')
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+socketio.on('connection', function(socket) {
+	connections.push(socket)
+	console.log("Connected: %s connections", connections.length)
+
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Disconnected: %s connections", connections.length)
+	})
+
+	socket.on('send message', function(data) {
+		console.log("Data: " + data);
+		socket.broadcast.emit('new message', data)
+	})
 })
