4eb2bab70c7cce1e882a25bb8cc84769a90a43c5
jefhai <jefhai@users.noreply.github.com>
2017-05-22 14:31:10 -0400

    added simple login page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const server = require('http').createServer(app)
 const socketio = require('socket.io').listen(server)
 const redis = require('redis')
 const redis_client = redis.createClient()
+const pug = require('pug')
+
+app.set('view engine', 'pug')
 
 connections = []
 
@@ -11,12 +14,14 @@ server.listen(3000, "0.0.0.0", function() {
   console.log('Server running... http://localhost:3000')
 })
 
+app.use('/static', express.static('public'))
+
 redis_client.on("error", function(err) {
     console.log("Error " + err)
 })
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.render('login')
 })
 
 socketio.on('connection', function(socket) {
