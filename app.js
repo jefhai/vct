64bbf644e0265fdda7ab4819a9b0221a5a6e78fe
jefhai <jefhai@users.noreply.github.com>
2017-05-24 09:31:08 -0400

    added semicolons

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const socketio = require('socket.io').listen(server)
-const redis = require('redis')
-const redis_client = redis.createClient()
-const pug = require('pug')
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const socketio = require('socket.io').listen(server);
+const redis = require('redis');
+const redis_client = redis.createClient();
+const pug = require('pug');
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-connections = []
+connections = [];
 
 server.listen(3000, "0.0.0.0", function() {
-	console.log('Server running... http://localhost:3000')
-})
+	console.log('Server running... http://localhost:3000');
+});
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
 
 redis_client.on("error", function(err) {
-	console.log("Error " + err)
-})
+	console.log("Error " + err);
+});
 
 app.get('/', function(req, res) {
-	res.render('login')
-})
+	res.render('login');
+});
 
 socketio.on('connection', function(socket) {
-	connections.push(socket)
-	console.log("Connected: %s connections", connections.length)
+	connections.push(socket);
+	console.log("Connected: %s connections", connections.length);
 
 	socket.on('disconnect', function(data) {
 		connections.splice(connections.indexOf(socket), 1);
-		console.log("Disconnected: %s connections", connections.length)
-	})
+		console.log("Disconnected: %s connections", connections.length);
+	});
 
 	socket.on('send message', function(data) {
 		console.log("Data: " + data);
-		socket.broadcast.emit('new message', data)
-	})
-})
+		socket.broadcast.emit('new message', data);
+	});
+});
