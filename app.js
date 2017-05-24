cb6fcbd5d82c2a987bae81313c635a5e45f17d78
jefhai <jefhai@users.noreply.github.com>
2017-05-24 09:39:58 -0400

    added routes, initialized passport

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const passport = require('passport');
+const local_strategy = require('passport-local').Strategy;
 const server = require('http').createServer(app);
 const socketio = require('socket.io').listen(server);
 const redis = require('redis');
 const redis_client = redis.createClient();
 const pug = require('pug');
 
+// Set Routes
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const gameboard = require('./routes/gameboard');
+
+var socket_connections = [];
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-connections = [];
+// Set Static Folder
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-server.listen(3000, "0.0.0.0", function() {
-	console.log('Server running... http://localhost:3000');
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+server.listen(app.get('port'), "0.0.0.0", function() {
+	console.log('Server running... http://localhost:'+app.get('port'));
 });
-
-app.use('/static', express.static('public'));
 
 redis_client.on("error", function(err) {
 	console.log("Error " + err);
-});
-
-app.get('/', function(req, res) {
-	res.render('login');
 });
 
 socketio.on('connection', function(socket) {
@@ -38,3 +51,7 @@ socketio.on('connection', function(socket) {
 		socket.broadcast.emit('new message', data);
 	});
 });
+
+app.use('/', routes);
+app.use('/', users);
+app.use('/', gameboard);
